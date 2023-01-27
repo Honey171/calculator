3dd01b07cef3647ef2ec59a4d973263730ac39be
Mehmet Taha <103601089+Honey171@users.noreply.github.com>
2023-01-27 18:55:28 +0300

    Update and rename App.js to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculatorBody from './components/CalculatorBody';
+
+function App() {
+  return (
+    <main className="flex justify-center items-center h-[100vh]">
+      <CalculatorBody />
+    </main>
+  );
+}
+
+export default App;
